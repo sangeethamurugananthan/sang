3770bc737cf111c7fd736e2ce078d9e6610acf8f
sangeethamurugananthan <sangeethakala.m.s@gmail.com>
2016-12-26 22:29:11 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class word
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String h=sc.nextLine();
+String j[]=h.split(" ");
+for(int i=length-1;i>=0;i--)
+{
+System.out.print(j[i]);
+System.out.print(" ");
+}
+}
+}
+
